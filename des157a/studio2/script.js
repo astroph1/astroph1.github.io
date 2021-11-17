339620c7044585ced6eff0b96c73d8392e66237b
starstruuk <rohitrameshofficial@gmail.com>
2021-11-17 15:08:40 -0800

    yes

--- des157a/studio2/script.js
+++ des157a/studio2/script.js
@@ -2,33 +2,56 @@
     'use strict';
     console.log('reading js');
 
+    const htmlTag = document.querySelector('html');
     const bodyTag = document.querySelector('body');
+    const btn = document.querySelector('button');
 
     document.getElementById('alien').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/alien.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/alien.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('pillow').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/coirm.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/coirm.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('knob').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/pump.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/pump.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('alien').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/alien.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/alien.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('crystal').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/room.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/room.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('spear').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/cave.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/cave.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
 
     document.getElementById('hat').addEventListener('click',function(event) {
-        bodyTag.style.backgroundImage = 'url("/des157a/Assets/cherry.png")';
+        htmlTag.style.backgroundImage = 'url("/des157a/Assets/cherry.png")';
+        htmlTag.style.backgroundRepeat = 'no-repeat';
+        callRevertButton();
     });
+
+    function callRevertButton() {
+        bodyTag.innerHTML += "<button id='return'>yo</button>";
+        document.getElementById("return").addEventListener('click',function(){
+            location.reload();
+        });
+    }
 })();
