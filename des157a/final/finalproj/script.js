f01bd1d3f37bdf15bc761ae5f862b6951f458b25
starstruuk <rohitrameshofficial@gmail.com>
2021-12-05 21:36:07 -0800

    final

--- des157a/final/finalproj/script.js
+++ des157a/final/finalproj/script.js
@@ -18,13 +18,20 @@
     var ilindex = 0;
     let cindex = 0;
     madlib.style.visibility = "hidden";
+    
     const preloader = document.getElementById('preloader');
-	preloader.className = 'fadeout';
-
-    preloader.addEventListener('animationend', function () {
+    const lBar = document.getElementById('myBar');
+    var width = 0;
+    setInterval(loadPage,10000);
+	
+    function loadPage() {
+        preloader.className = 'fadeout';
+        preloader.addEventListener('animationend', function () {
         
 		preloader.style.display = 'none';
 	});
+    }
+    
 
     convoBtn.addEventListener('click',function(){
        if (cindex<convoList.length) 
@@ -35,11 +42,10 @@
             <label for="word">Name of the hero</label>
             <br />
             <input type="text" id="word" required>
-            <input type="submit" value="Ok" id="okay">
+            <input type="submit" value="Done" id="okay">
             </form>`;
             myForm.style.top = "206px";
             myForm.style.left = "784px";
-            form.style.animation = "pop-up 2s forwards";
         }
     });
 
@@ -63,13 +69,14 @@
             footTag.style.visibility = "hidden";
             myForm.style.visibility = "hidden";
             madlib.style.visibility = "visible";
+            madlib.style.animation = "pop-in 1.5s forwards";
             madlib.innerHTML+= `<button id="close">X</button><p>Let me tell you the story of <b>${wordList[0]}</b> the fearless <br/>
             A short-statured paladin nonetheless</p><p>Instructed by the Emperor to slay <b>${wordList[1]}</b> the warlock <br/>
-            Unperturbed, <b>${wordList[0]}</b> left towards his lair at the shout of the human clock</p><br/><p>'Twas a glorious battle, of blood, sweat and tears <br/>
+            Unperturbed, <b>${wordList[0]}</b> left towards his lair at the shout of the human clock</p><p>'Twas a glorious battle, of blood, sweat and tears <br/>
             Brandishing the holy <b>${wordList[2]}</b>, amidst their companions' cheers
-            </p><br/><p>In a fit of rage, <b>${wordList[1]}</b> attacked <b>${wordList[3]}</b>, <b>${wordList[0]}</b>'s closest companion <br/>
-            A last-ditch attempt to get back at the paladin, nearly sending their companion to oblivion</p><br/><p>But <b>${wordList[0]}</b> intervened just in the nick of time,<br/>
-            And the attack to finish <b>${wordList[2]}</b> was just sublime</p><br/><p>The paladin changed their name to Julius Caeser <br/>
+            </p><p>In a fit of rage, <b>${wordList[1]}</b> attacked <b>${wordList[3]}</b>, <b>${wordList[0]}</b>'s closest companion <br/>
+            A last-ditch attempt to get back at the paladin, nearly sending their companion to oblivion</p><p>But <b>${wordList[0]}</b> intervened just in the nick of time,<br/>
+            And the attack to finish <b>${wordList[2]}</b> was just sublime</p><p>The paladin changed their name to Julius Caeser <br/>
             Well, I wish, <b>${wordList[1]}</b> later succumbed to hayfever</p>`;
             bodyTag.style.cssText+= "background: rgb(0, 0, 0, 0.7); background-image: url('/des157a/Assets/illus-madlibs.png'); background-blend-mode: darken; background-repeat: no-repeat; width: 1366px; max-height: 768px; position: absolute; margin: auto; ";
             let closeBtn = document.querySelector('#close');
@@ -87,8 +94,7 @@
         footTag.style.visibility = "visible";
         myForm.innerHTML = `<div class="convo">
             <p id="conver">So, you wanna write another one?</p>
-            <button id="replay">Yes</button>
-            <button id="quit">No</button>
+            <button id="replay">Retry</button>
         </div>`;
         bodyTag.style.cssText= "background-image: url('/des157a/Assets/illus-madlibs.png'); background-repeat: no-repeat; width: 1366px; max-height: 768px; position: absolute; margin: auto; ";
         myForm.style.top = "67px";
@@ -96,10 +102,6 @@
         let restartBtn = document.querySelector('#replay');
             restartBtn.addEventListener('click',function(){
                location.reload();
-        })
-        let closeBtn = document.querySelector('#close');
-        closeBtn.addEventListener('click',function(){
-            console.log('this is just a dud button');
         })
     }
 
